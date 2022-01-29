b720587fa445e52f88fe8ad54a233eff051e0597
Brent Westbrook <brentrwestbrook@gmail.com>
2022-01-29 10:32:15 -0600

    use emojis for high and low

--- main.go
+++ main.go
@@ -130,7 +130,7 @@ func main() {
 			low = p.Temperature
 		}
 	}
-	fmt.Printf("Hi:%d Lo:%d Cur:%+d°F %s %s \n",
+	fmt.Printf("🔺%d 🔻%d Cur:%+d°F %s %s \n",
 		high, low,
 		now.Temperature,
 		now.WindDirection, now.WindSpeed,
